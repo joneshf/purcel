b3365dcfb3533c70a24c53b4f749d7de39879752
joneshf <jones3.hardy@gmail.com>
2018-02-03 21:41:06 -0800

    Setup RIO
    Bit more verbose than I'd like for a Hello World,
    but the end goal isn't to make a Hello World program.
    The end goal is going to be much more complex.
    
    We're betting that this initial setup pays off in the end.

--- src/Main.hs
+++ src/Main.hs
@@ -3,4 +3,31 @@ module Main where
 import "rio" RIO
 
 main :: IO ()
-main = pure ()
+main = runRIO devEnv main'
+
+main' :: (HasLogFunc env) => RIO env ()
+main' = do
+  logInfo "hello world"
+
+data Env
+  = Env
+    { envLogFunc :: !LogFunc
+    }
+
+instance HasLogFunc Env where
+  logFuncL = to envLogFunc
+
+devEnv :: Env
+devEnv = Env { envLogFunc }
+  where
+  envLogFunc cs source level str =
+    withStickyLogger options $ \f -> f cs source level str
+    where
+    options = LogOptions
+      { logMinLevel = LevelDebug
+      , logTerminal = True
+      , logUseColor = True
+      , logUseTime = False
+      , logUseUnicode = True
+      , logVerboseFormat = True
+      }
